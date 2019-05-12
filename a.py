fa93fb3199962666131e9938200fd75228b5d8c0
xwp <xwp_fullstack@163.com>
2019-05-12 21:35:16 +0800

    重新排序

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 def auto():
     for i in range(1,10):
         for j in range(1,i+1):
-                print("%dx%d=%-3d"%(i,j,i*j),end='')
+                print("%dx%d=%-3d"%(j,i,i*j),end='')
         print()
 auto()
